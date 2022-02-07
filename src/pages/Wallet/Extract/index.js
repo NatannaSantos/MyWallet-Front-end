08cf295f678e7dc936e7d779179fc26bc1b1b31e
Natanna <natannacortez@gmail.com>
2022-02-07 16:30:18 -0300

    refactor: refactoring code

--- src/pages/Wallet/Extract/index.js
+++ src/pages/Wallet/Extract/index.js
@@ -2,7 +2,7 @@ import { ButtonDelete } from "../style";
 import { Container, Description, Value, Date } from "./style";
 
 function Extract({_id,date, description,value,type,handleDeleteTransaction}){
-   const values=parseFloat(value.replace(',',',')).toFixed(2);
+   const values=parseFloat(value.replace(',','.')).toFixed(2);
       
     return(
         <Container>
